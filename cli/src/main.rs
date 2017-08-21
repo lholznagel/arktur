a65c50f4b9cf78768491c4214a764ddeed50b6b3
lholznagel <lars.holznagel@outlook.com>
2017-08-21 22:01:09 +0200

    added generation of public and private keys

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,9 @@
+extern crate openssl;
+
+use openssl::rsa::Rsa;
+
 fn main() {
-    println!("Hello, world!");
+    let pripub = Rsa::generate(4096).unwrap();
+    println!("{:?}", String::from_utf8(pripub.public_key_to_pem().unwrap()));
+    println!("{:?}", String::from_utf8(pripub.private_key_to_pem().unwrap()));
 }
