b7c1f2a7f3f5a5d68cc1f8741c49bebfb45f76b3
lholznagel <lars.holznagel@outlook.com>
2017-09-08 10:04:03 +0200

    removed sql insertions on startup

--- peer/src/connections/postgres.rs
+++ peer/src/connections/postgres.rs
@@ -1,37 +1,16 @@
-use r2d2::PooledConnection;
+use config::Database;
 use r2d2;
 use r2d2_postgres::{PostgresConnectionManager, TlsMode};
-use std::fs::File;
-use std::io::Read;
 
 type Pool = r2d2::Pool<PostgresConnectionManager>;
 
-pub fn init() -> Pool {
+pub fn init(config: &Database) -> Pool {
     let config_r2d2 = r2d2::Config::default();
+
+    let connection_string = format!("postgres://{}:{}@{}:{}", config.user, config.password, config.address, config.port);
     let manager =
-        PostgresConnectionManager::new("postgres://postgres:password@localhost", TlsMode::None)
+        PostgresConnectionManager::new(connection_string, TlsMode::None)
             .unwrap();
 
-    let pool = r2d2::Pool::new(config_r2d2, manager).unwrap();
-
-    info!("Preparing database");
-    pool.get().unwrap().execute("CREATE EXTENSION IF NOT EXISTS pgcrypto;", &[]).unwrap();
-
-    prepare_database(pool.get().unwrap(), "queries/create_block.sql");
-    prepare_database(pool.get().unwrap(), "queries/create_blockchain.sql");
-    prepare_database(pool.get().unwrap(), "queries/create_peer.sql");
-    info!("Done with preparing database");
-
-    return pool;
-}
-
-fn prepare_database(db: PooledConnection<PostgresConnectionManager>, name: &str) {
-    let mut file = File::open(name).unwrap();
-    let mut content = String::new();
-
-    file.read_to_string(&mut content).expect(
-        "Could not read preparation script",
-    );
-
-    db.execute(content.as_str(), &[]).unwrap();
+    r2d2::Pool::new(config_r2d2, manager).unwrap()
 }
