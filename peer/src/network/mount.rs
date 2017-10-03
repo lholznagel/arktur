e35a58ad62fef7b7290d2050a320ab98ffed77c9
lholznagel <lars.holznagel@outlook.com>
2017-10-03 17:45:38 +0200

    extracted postgres connection to own file

--- peer/src/network/mount.rs
+++ peer/src/network/mount.rs
@@ -2,9 +2,9 @@ use iron::prelude::{Request, Response, IronResult};
 use iron::status;
 use persistent::Read;
 use plugin::Pluggable;
-use connection::{Database, PostgresPool};
+use connection::Database;
 
-pub fn foo(req: &mut Request) -> IronResult<Response> {
+pub fn foo() -> IronResult<Response> {
     let pool = req.get::<Read<Database>>().unwrap();
     pool.get().unwrap().query("SELECT * FROM peers", &[]).unwrap();
     Ok(Response::with((status::Ok, "Hello foo")))
