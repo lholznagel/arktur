b96489d7862a2ed9af88a57ff4cf1839c607d3f1
lholznagel <lars.holznagel@outlook.com>
2017-10-10 22:24:01 +0200

    some progress

---
+++ network/src/lib.rs
@@ -0,0 +1,28 @@
+use std::str;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, UdpSocket, SocketAddrV6};
+
+#[derive(Debug)]
+pub struct NetworkConnect {
+    ip: Ipv6Addr,
+    port: u16,
+}
+
+impl NetworkConnect {
+    pub fn new() -> Self {
+        NetworkConnect {
+            ip: Ipv4Addr::new(0, 0, 0, 0).to_ipv6_mapped(),
+            port: 0
+        }
+    }
+
+    pub fn start(mut self) -> UdpSocket {
+        let socket = SocketAddrV6::new(self.ip, self.port, 0, 0);
+        let socket = UdpSocket::bind(socket).unwrap();
+        self.port = socket.local_addr().unwrap().port();
+        socket
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+}
