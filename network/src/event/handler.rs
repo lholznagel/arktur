8783652029831a8519637a9d48dd30ee82272b16
lholznagel <lars.holznagel@outlook.com>
2017-10-21 11:29:12 +0200

    updated api

--- network/src/event/handler.rs
+++ network/src/event/handler.rs
@@ -3,36 +3,54 @@ use std::net::SocketAddr;
 /// Holds all handler
 pub struct EventHandler {
     /// TODO: documentation
-    pub register_handler: fn(SocketAddr, String) -> &'static str,
+    pub ping_handler: fn(SocketAddr, &str) -> &'static str,
     /// TODO: documentation
-    pub register_ack_handler: fn(SocketAddr, String) -> &'static str
+    pub pong_handler: fn(SocketAddr, &str) -> &'static str,
+    /// TODO: documentation
+    pub register_ack_handler: fn(SocketAddr, &str) -> &'static str,
+    /// TODO: documentation
+    pub register_handler: fn(SocketAddr, &str) -> &'static str
 }
 
 /// Contains all handler for events
 impl EventHandler {
     /// Creates a new instance of handlers
     pub fn new() -> Self {
-        fn empty(_: SocketAddr, _: String) -> &'static str { "" }
+        fn empty(_: SocketAddr, _: &str) -> &'static str { "" }
 
         EventHandler {
-            register_handler: empty,
-            register_ack_handler: empty
+            ping_handler: empty,
+            pong_handler: empty,
+            register_ack_handler: empty,
+            register_handler: empty
         }
     }
 
-    /// Registeres a new handler that fires on `REGISTER`
-    ///
-    /// TODO: more docu
-    pub fn set_register_handler(mut self, function: fn(SocketAddr, String) -> &'static str) -> Self {
-        self.register_handler = function;
+    /// TODO: documentation
+    pub fn set_ping_handler(mut self, function: fn(SocketAddr, &str) -> &'static str) -> Self {
+        self.ping_handler = function;
+        self
+    }
+
+    /// TODO: documentation
+    pub fn set_pong_handler(mut self, function: fn(SocketAddr, &str) -> &'static str) -> Self {
+        self.pong_handler = function;
         self
     }
 
     /// Registeres a new handler that fires on `REGISTER_ACK`
     ///
     /// TODO: more docu
-    pub fn set_register_ack_handler(mut self, function: fn(SocketAddr, String) -> &'static str) -> Self {
+    pub fn set_register_ack_handler(mut self, function: fn(SocketAddr, &str) -> &'static str) -> Self {
         self.register_ack_handler = function;
+        self
+    }
+
+    /// Registeres a new handler that fires on `REGISTER`
+    ///
+    /// TODO: more docu
+    pub fn set_register_handler(mut self, function: fn(SocketAddr, &str) -> &'static str) -> Self {
+        self.register_handler = function;
         self
     }
 }
