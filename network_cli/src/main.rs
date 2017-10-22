663d274ee30e9519aa16b5268ad633a524fcd2bf
lholznagel <lars.holznagel@outlook.com>
2017-10-22 22:24:58 +0200

    some more progress

--- network_cli/src/main.rs
+++ network_cli/src/main.rs
@@ -6,21 +6,43 @@ use blockchain_network::event::EventHandler;
 
 use clap::{Arg, App};
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
+use std::net::{SocketAddr, UdpSocket};
 
 fn main() {
+    let matches = App::new("Blochain network cli")
+        .version("0.1.0")
+        .author("Lars Holznagel")
+        .about("Client tool using the blockchain_network library")
+        .arg(Arg::with_name("HOLE_PUNCHER_IP")
+            .value_name("ip")
+            .help("Sets the IP of the Hole puncher service")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("HOLE_PUNCHER_PORT")
+            .value_name("port")
+            .help("Sets the port of the Hole puncher service.")
+            .takes_value(true)
+            .default_value("45000"))
+        .get_matches();
+
+    let mut combined = String::from("");
+    combined.push_str(matches.value_of("HOLE_PUNCHER_IP").unwrap());
+    combined.push_str(":");
+    combined.push_str(matches.value_of("HOLE_PUNCHER_PORT").unwrap());
+    println!("{:?}", combined);
+    connect(combined.parse::<SocketAddr>().unwrap());
+}
+
+fn connect(addr: SocketAddr) {
     let event_handler = EventHandler::new();
     let event_handler = event_handler
         .set_ping_handler(ping_handler)
         .set_pong_handler(pong_handler)
         .set_register_ack_handler(register_ack_handler);
 
-    //UdpClientBuilder::new(event_handler);
     let udp_client = UdpClientBuilder::new().build(event_handler);
-    let udp_client = udp_client.notify_hole_puncher(SocketAddr::new(IpAddr::from(Ipv4Addr::new(0,0,0,0)), 45000));
+    let udp_client = udp_client.notify_hole_puncher(addr);
     udp_client.listen();
-
-    //build_command();
 }
 
 fn register_ack_handler(_: SocketAddr, udp: &UdpSocket, message: &str) {
@@ -29,7 +51,7 @@ fn register_ack_handler(_: SocketAddr, udp: &UdpSocket, message: &str) {
      if message.replace("ACK_REGISTER | ", "") == "NO_PEER" {
          println!("No peer");
      } else {
-        println!("Peer: {}", message.replace("ACK_REGISTER | ", ""));
+        println!("Sending PING to Peer: {}", message.replace("ACK_REGISTER | ", ""));
         udp.send_to(b"PING |", message.replace("ACK_REGISTER | ", "").parse::<SocketAddr>().unwrap()).unwrap();
      }
 }
@@ -42,20 +64,4 @@ fn ping_handler(source: SocketAddr, udp: &UdpSocket, _: &str) {
 
 fn pong_handler(source: SocketAddr, _: &UdpSocket, _: &str) {
     println!("PONG from {:?}", source.to_string());
-}
-
-fn build_command() {
-    App::new("Blochain network cli")
-        .version("0.1.0")
-        .author("Lars Holznagel")
-        .about("Client tool using the blockchain_network library")
-        .arg(Arg::with_name("ip")
-            .help("Sets the IP of the Hole puncher service")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("port")
-            .help("Sets the port of the Hole puncher service.")
-            .takes_value(true)
-            .default_value("45000"))
-        .get_matches();
 }
