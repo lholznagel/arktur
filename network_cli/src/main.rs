2cd5070a0d58182058c5e0ce42a4f465bdb5f577
lholznagel <lars.holznagel@outlook.com>
2017-10-25 09:06:03 +0200

    extracted handlers to own file

--- network_cli/src/main.rs
+++ network_cli/src/main.rs
@@ -8,7 +8,9 @@ use blockchain_network::event::EventHandler;
 
 use clap::{Arg, App};
 
-use std::net::{SocketAddr, UdpSocket};
+use std::net::SocketAddr;
+
+pub mod handlers;
 
 fn main() {
     let matches = App::new("Blochain network cli")
@@ -38,40 +40,12 @@ fn main() {
 fn connect(addr: SocketAddr) {
     let event_handler = EventHandler::new();
     let event_handler = event_handler
-        .set_ping_handler(ping_handler)
-        .set_pong_handler(pong_handler)
-        .set_peer_registering_handler(peer_registering_handler)
-        .set_register_ack_handler(register_ack_handler);
+        .set_ping_handler(handlers::ping_handler)
+        .set_pong_handler(handlers::pong_handler)
+        .set_peer_registering_handler(handlers::peer_registering_handler)
+        .set_register_ack_handler(handlers::register_ack_handler);
 
     let udp_client = UdpClientBuilder::new().build(event_handler);
     let udp_client = udp_client.notify_hole_puncher(addr);
     udp_client.listen();
-}
-
-fn ping_handler(source: SocketAddr, udp: &UdpSocket, _: &str) {
-    event!(format!("PING from peer {:?}", source.to_string()));
-    sending!(format!("PONG to peer {:?}", source.to_string()));
-    udp.send_to(b"PONG |", source).unwrap();
-    success!("Send PONG");
-}
-
-fn pong_handler(source: SocketAddr, _: &UdpSocket, _: &str) {
-    event!(format!("PONG from peer {:?}", source.to_string()));
-}
-
-fn peer_registering_handler(_: SocketAddr, udp: &UdpSocket, message: &str) {
-    event!(format!("PEER_REGISTERING {:?}", message.replace("PEER_REGISTERING | ", "")));
-    sending!(format!("PING to new peer {:?}", message.replace("PEER_REGISTERING | ", "")));
-    udp.send_to(b"PING |", message.replace("PEER_REGISTERING | ", "").parse::<SocketAddr>().unwrap()).unwrap();
-    success!(format!("Send PING {:?}", message.replace("PEER_REGISTERING | ", "")));
-}
-
-fn register_ack_handler(_: SocketAddr, udp: &UdpSocket, message: &str) {
-     if message.replace("ACK_REGISTER | ", "") == "NO_PEER" {
-         error!("No peer");
-     } else {
-        sending!(format!("PONG to peer {:?}", message.replace("ACK_REGISTER | ", "")));
-        udp.send_to(b"PING |", message.replace("ACK_REGISTER | ", "").parse::<SocketAddr>().unwrap()).unwrap();
-        success!(format!("Send PING to {:?}", message.replace("ACK_REGISTER | ", "")));
-     }
 }
