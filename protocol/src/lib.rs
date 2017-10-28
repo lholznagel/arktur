975e6ea3e9d806f83066d4018dae9a091bb9b651
lholznagel <lars.holznagel@outlook.com>
2017-10-28 13:02:34 +0200

    added parsing of string hex

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate nom;
 
-const CHARS: &'static [u8] = b"01234567890ABCDEF";
+pub mod hex;
 
 named!(parse_protocol<&[u8], (u8, u8, u16, u16, u16)>, bits!(tuple!(take_bits!(u8, 8), take_bits!(u8, 8), take_bits!(u16, 16), take_bits!(u16, 16), take_bits!(u16, 16))));
 
@@ -26,22 +26,10 @@ pub fn parse(to_parse: &[u8]) -> BlockchainProtocol {
     }
 }
 
-pub fn to_hex(bytes: &[u8]) -> String {
-    let mut v = Vec::with_capacity(bytes.len() * 2);
-
-    for &byte in bytes {
-        v.push(CHARS[(byte >> 4) as usize]);
-        v.push(CHARS[(byte &0xf) as usize]);
-    }
-
-    unsafe {
-        String::from_utf8_unchecked(v)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hex::*;
 
     #[test]
     fn test_simple_u8() {
@@ -70,6 +58,24 @@ mod tests {
 
         let data = &[0x01, 0x02, 0xFF, 0xFF, 0x05, 0x39, 0x00, 0x00];
         let result = parse(data);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_simple_string_u8() {
+        let expected = BlockchainProtocol {
+            event_code: 1,
+            status_code: 2,
+            id: 65535,
+            ttl: 1337,
+            data_length: 0
+        };
+
+        let data = &[1, 2, 255, 255, 5, 57, 0, 0];
+        let data = to_hex(data);
+        println!("{:?}", data);
+        let data = from_hex(data.as_str());
+        let result = parse(data.as_slice());
         assert_eq!(result, expected);
     }
 }
