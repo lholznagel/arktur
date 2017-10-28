e1d21136399d61a70b41864e3a1f6625022c719b
lholznagel <lars.holznagel@outlook.com>
2017-10-29 01:58:32 +0200

    resturctured protocol folder

---
+++ protocol/src/protocol.rs
@@ -0,0 +1,36 @@
+//! Contains the protocol model and a builder for the protocol
+use enums::events::EventCodes;
+
+/// Struct of the protocol
+///
+/// ```
+/// //  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// // | Eventcode             | Status                |                 ID                            |
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// // |                 TTL                           |                 Data length                   |
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// // |                                                                                               |
+/// // //                                                                                             //
+/// // //                Data                                                                         //
+/// // //                                                                                             //
+/// // |                                                                                               |
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// ```
+///
+/// - TODO: add checksum
+#[derive(Debug, PartialEq)]
+pub struct BlockchainProtocol {
+    /// Event that is fired, defined by a number between 0 and 255
+    pub event_code: EventCodes,
+    /// Status of this message, defined by a number between 0 and 255
+    pub status_code: u8,
+    /// Identification of this message
+    pub id: u16,
+    /// TTL of this message
+    pub ttl: u16,
+    /// Length of the added data field
+    pub data_length: u16,
+    /// Contains the content of the data field
+    pub data: String,
+}
