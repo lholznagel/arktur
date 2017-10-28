93bd613282ccf97c8fedbe46c935245f4f9481f6
lholznagel <lars.holznagel@outlook.com>
2017-10-28 18:57:09 +0200

    updated license
    added missing reference to original in hex.rs file, sorry for that

--- protocol/src/hex.rs
+++ protocol/src/hex.rs
@@ -1,3 +1,14 @@
+// Most of the code is taken from https://github.com/rust-lang-deprecated/rustc-serialize/blob/master/src/hex.rs
+//
+// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this (being https://github.com/rust-lang-deprecated/rustc-serialize)
+// distribution and at http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 const CHARS: &'static [u8] = b"0123456789ABCDEF";
 
 pub fn to_hex(bytes: &[u8]) -> String {
