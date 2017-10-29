4179088ed3d252cb16a5d0e41c19bfccbd07e07b
lholznagel <lars.holznagel@outlook.com>
2017-10-29 22:07:12 +0100

    added protocol builder

--- protocol/src/protocol.rs
+++ protocol/src/protocol.rs
@@ -1,5 +1,6 @@
 //! Contains the protocol model and a builder for the protocol
-use enums::events::EventCodes;
+use enums::events::{as_number, EventCodes};
+use std::{slice, mem};
 
 /// Struct of the protocol
 ///
@@ -33,4 +34,77 @@ pub struct BlockchainProtocol {
     pub data_length: u16,
     /// Contains the content of the data field
     pub data: String,
+}
+
+impl BlockchainProtocol {
+    /// Creates a new instance of the protocol information
+    pub fn new() -> Self {
+        BlockchainProtocol {
+            event_code: EventCodes::NotAValidEvent,
+            status_code: 0,
+            id: 0,
+            ttl: 0,
+            data_length: 0,
+            data: String::from(""),
+        }
+    }
+
+    /// Sets the event code
+    ///
+    /// # Default
+    ///
+    /// EventCodes::NotAValidEvent
+    ///
+    /// # Parameters
+    ///
+    /// - `data` - payload that should be send
+    ///
+    /// # Return
+    ///
+    /// Updated instance of the struct
+    pub fn set_event_code(mut self, event_code: EventCodes) -> Self {
+        self.event_code = event_code;
+        self
+    }
+
+    /// Sets the data that should be send as payload
+    ///
+    /// # Default
+    ///
+    /// Empty String
+    ///
+    /// # Parameters
+    ///
+    /// - `data` - payload that should be send
+    ///
+    /// # Return
+    ///
+    /// Updated instance of the struct
+    pub fn set_data(mut self, data: String) -> Self {
+        self.data = data.clone();
+        self.data_length = data.len().to_string().parse::<u16>().unwrap();
+        self
+    }
+
+    /// Combines the struct to a vector of bytes
+    pub fn build(self) -> Vec<u8> {
+        let slice_u16: &[u16] = &*vec![self.id, self.ttl, self.data_length];
+        let converted_slice: &[u8] = unsafe {
+            slice::from_raw_parts(
+                slice_u16.as_ptr() as *const u8,
+                slice_u16.len() * mem::size_of::<u16>(),
+            )
+        };
+
+        let mut result: Vec<u8> = Vec::new();
+        result.push(as_number(self.event_code));
+        result.push(self.status_code);
+        result.push(converted_slice[0]);
+        result.push(converted_slice[1]);
+        result.push(converted_slice[2]);
+        result.push(converted_slice[3]);
+        result.push(converted_slice[4]);
+        result.push(converted_slice[5]);
+        result
+    }
 }
