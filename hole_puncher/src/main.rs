98a6d935d4b52c074d06e7c6e904fc93ff3c22ea
lholznagel <lars.holznagel@outlook.com>
2017-11-03 00:08:23 +0100

    added more documentation

--- hole_puncher/src/main.rs
+++ hole_puncher/src/main.rs
@@ -1,15 +1,57 @@
 #![deny(missing_docs)]
 
-//! Hole puncher service
+//! # What does it do?
 //!
-//! Saved the last address that registered itself
+//! - Create a "hole" between to peers
+//! - When a peer registers itself, its IP-Address + Port are saved
+//! - The next peer that registers itself, gets these IP-Address + Port
+//! - The older peer gets the IP-Address + Port of the new peer
+//! - The address of the new peer are saved instead of the old peer
+//! - Both start a ping event to the other peer
+//! - With this technic a connection between two private networks can be accomplished
+//!
+//! In the following graphic, the process is shown
+//!
+//! ```
+//!  1. Register  +--------------+ 2. Register
+//!   +--------->|              |<---------+
+//!   |          | hole puncher |          |
+//!   |    +-----|              |-----+    |
+//!   |    |     +--------------+     |    |
+//!   |    | 3. Send IP+Port of new   |    |
+//!   |    |                          |    |
+//!   |    |                          |    |
+//!   |    |                          |    |
+//!   |    |   4. Send IP+Port of old |    |
+//!   |    v                          v    |
+//! +--------+                      +--------+
+//! |        |--------------------->|        |
+//! | Peer A |      5. Contact      | Peer B |
+//! |        |<---------------------|        |
+//! +--------+                      +--------+
+//!
+//! created with http://asciiflow.com/
+//! ```
+//!
+//! # Example
+//!
+//! - Peer A runs on 192.168.1.5:45678 (on host a)
+//! - Peer B runs on 192.168.1.6:56789 (on host b)
+//! - Peer A registers itself at the hole puncher (some.public.ip.address:45000)
+//! - The hole puncher does not know any peer
+//! - Peer B registers itself at the same hole puncher
+//! - The hole puncher sends the Peer B information to Peer A
+//! - The hole puncher then sends the Peer A information to Peer B
+//! - Peer A and Peer B try to ping each other
+//! - The connection between both networks should be good to go
+
 #[macro_use]
 extern crate blockchain_logging;
 extern crate blockchain_network;
 extern crate blockchain_protocol;
 
-use blockchain_network::udp_client::UdpClientBuilder;
 use blockchain_network::event::EventHandler;
+use blockchain_network::udp_client::UdpClientBuilder;
 use blockchain_protocol::BlockchainProtocol;
 use blockchain_protocol::enums::events::EventCodes;
 
@@ -18,6 +60,8 @@ use std::io::{Read, Write};
 use std::net::{UdpSocket, SocketAddr};
 
 /// Starting point
+///
+/// Registers all needed event handlers and starts a UDP-Listener
 fn main() {
     remove_file("last_peer").unwrap();
     File::create("last_peer").unwrap();
@@ -57,8 +101,10 @@ fn register_handler(source: SocketAddr, udp: &UdpSocket, _: BlockchainProtocol) 
     file.write_all(source.to_string().as_bytes()).unwrap();
 
     sending!(format!("ACK_REGISTER | {}", response));
-    
-    let message = BlockchainProtocol::new().set_event_code(EventCodes::AckRegister).set_data(String::from(response)).build();
-    udp.send_to(message.as_slice(), source)
-        .unwrap();
+
+    let message = BlockchainProtocol::new()
+        .set_event_code(EventCodes::AckRegister)
+        .set_data(String::from(response))
+        .build();
+    udp.send_to(message.as_slice(), source).unwrap();
 }
