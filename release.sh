be661416ae791aceea30937f8a2a148d3ba33d49
lholznagel <github@lholznagel.info>
2017-12-18 00:45:37 +0100

    issue_2 Added makefile

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
+#!/bin/sh
+
 FILES=$(find target/release -maxdepth 1 -name "blockchain_*" -executable)
 
 for file in $FILES
 do
   echo "Current file: $file"
   echo "Size: $(du -h $file | cut -f -1)"
-  echo "Stripping just for you"
+  echo "Stripping, just for you"
   strip $file
   echo "Done. New size: $(du -h $file | cut -f -1)"
+  echo ""
 done
