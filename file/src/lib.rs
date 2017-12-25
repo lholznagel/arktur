cc0b2f603fc1e3bbb0154caded0917ca9d62725d
lholznagel <github@lholznagel.info>
2017-12-25 20:47:44 +0100

    issue_6 Extended file crate

--- file/src/lib.rs
+++ file/src/lib.rs
@@ -1,4 +1,8 @@
 #![deny(missing_docs)]
 
-//! Handles everything that has to do with peers
+//! Handles reading and writing
+
+/// Handles everything that has to do with blocks
+pub mod blocks;
+/// Handles everything that has to do with peers
 pub mod peers;
