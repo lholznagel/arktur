200ab299f2b0875b7bb8d03a48fc11648c78fc3c
lholznagel <github@lholznagel.info>
2018-01-10 19:07:57 +0100

    protocol - Removed benchmark

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,12 +1,9 @@
 #![deny(missing_docs)]
-#![feature(test)]
 
 //! Helper library for parsing the protocol (still needs a better name) that is used in this project
 #[cfg(test)]
 #[macro_use]
 extern crate quickcheck;
-#[cfg(test)]
-extern crate test;
 
 extern crate blockchain_hooks;
 extern crate crc;
