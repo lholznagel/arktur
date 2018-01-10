0ef9a3dc361ce420e2cb4f5c14bb41d30dc03928
lholznagel <github@lholznagel.info>
2018-01-10 18:49:06 +0100

    protocol - Added quickcheck for FoundBlockPayload

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,14 +1,15 @@
 #![deny(missing_docs)]
+#![feature(test)]
 
 //! Helper library for parsing the protocol (still needs a better name) that is used in this project
-
 #[cfg(test)]
 #[macro_use]
 extern crate quickcheck;
-
-extern crate crc;
+#[cfg(test)]
+extern crate test;
 
 extern crate blockchain_hooks;
+extern crate crc;
 #[macro_use]
 extern crate nom;
 extern crate time;
