fba487cebd6671c83fc3aefc879b94112a621ab2
lholznagel <github@lholznagel.info>
2018-01-13 18:01:19 +0100

    protcol - Added tests for ValidatedHashPayload

--- protocol/src/payload/block/validated_hash.rs
+++ protocol/src/payload/block/validated_hash.rs
@@ -1,7 +1,20 @@
 use payload::PayloadModel;
+use payload::{ByteBuilder, Parser};
+
 use std::str;
 
 /// Model for the event `FoundBlock`
+///
+/// ```
+/// //  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// // | Index (unsigned)                                                                              |
+/// // |                                                                                               |
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// // | Hash                                                                                          |
+/// // |                                                                                               |
+/// // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// ```
 #[derive(Debug, PartialEq)]
 pub struct ValidatedHash {
     /// Index of the block
@@ -19,9 +32,13 @@ impl PayloadModel for ValidatedHash {
     }
 
     fn parse(bytes: Vec<Vec<u8>>) -> Self {
-        Self {
-            index: String::from(str::from_utf8(&bytes[0]).unwrap()).parse::<u64>().unwrap(),
-            hash: String::from(str::from_utf8(&bytes[1]).unwrap())
+        if !bytes.is_empty() {
+            Self {
+                index: Parser::u8_to_u64(bytes[0].as_slice()),
+                hash: String::from(str::from_utf8(&bytes[1]).unwrap())
+            }
+        } else {
+            Self::new()
         }
     }
 
@@ -30,18 +47,54 @@ impl PayloadModel for ValidatedHash {
     }
 
     fn as_bytes(self) -> Vec<u8> {
-        let mut result = Vec::<u8>::new();
-        result.push(126);
-        for i in self.index.to_string().into_bytes() {
-            result.push(i);
-        }
-        result.push(126);
+        ByteBuilder::new()
+            .add_u64(self.index)
+            .add_string(self.hash)
+            .build()
+    }
+}
 
-        result.push(126);
-        for i in self.hash.into_bytes() {
-            result.push(i);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use payload::Parser;
+
+    #[test]
+    fn test_building_and_parsing() {
+        let index = 65485;
+        let hash = String::from("ngiurengoiurehgbiuergneoigjoierhg");
+
+        let validated_hash = ValidatedHash {
+            index: index.clone(),
+            hash: hash.clone()
+        };
+
+        let validated_hash = validated_hash.as_bytes();
+        let complete = Parser::parse_payload(&validated_hash);
+        let parsed = ValidatedHash::parse(complete);
+
+        assert_eq!(index, parsed.index);
+        assert_eq!(hash, parsed.hash);
+    }
+
+    quickcheck! {
+        fn test_quickcheck(index: u64, hash: String) -> bool {
+            let index = index;
+            let hash = hash;
+
+            let validated_hash = ValidatedHash {
+                index: index.clone(),
+                hash: hash.clone()
+            };
+
+            let validated_hash = validated_hash.as_bytes();
+
+            let complete = Parser::parse_payload(&validated_hash);
+            let parsed = ValidatedHash::parse(complete);
+
+            assert_eq!(index, parsed.index);
+            assert_eq!(hash, parsed.hash);
+            true
         }
-        result.push(126);
-        result
     }
 }
