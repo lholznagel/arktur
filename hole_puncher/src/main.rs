2b1b9f6ebb86accb5ef4be6529a4a355b9c471e4
lholznagel <github@lholznagel.info>
2018-01-16 19:51:15 +0100

    hole_puncher - Init implementatio

--- hole_puncher/src/main.rs
+++ hole_puncher/src/main.rs
@@ -1,3 +1,35 @@
+#![deny(missing_docs)]
+
+//! Hole puncher service
+//!
+//! Initiates a connection between two peers behind a NAT
+//! Also saves all registered peers and provides them to new peers
+//!
+//! Default port: 50000
+extern crate blockchain_hooks;
+#[macro_use]
+extern crate blockchain_logging;
+extern crate blockchain_network;
+
+mod hooks;
+
+use blockchain_hooks::HookRegister;
+use blockchain_network::udp_client::UdpClientBuilder;
+
+const UDP_PORT: u16 = 50000;
+
 fn main() {
-    println!("Hello, world!");
+    info!("Starting hole puncher on port {}", UDP_PORT);
+    connect();
+}
+
+fn connect() {
+    let hook_notification = HookRegister::new()
+        .set_hook(hooks::HookHandler)
+        .get_notification();
+
+    UdpClientBuilder::new()
+        .set_port(UDP_PORT)
+        .build(hook_notification)
+        .listen();
 }
