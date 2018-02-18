0d72f3048516de8ba99cf4a24565a5ceeb42e58a
lholznagel <github@lholznagel.info>
2018-02-18 17:28:32 +0100

    hole_puncher - Removed file crate

--- peer/src/hooks/found_block.rs
+++ peer/src/hooks/found_block.rs
@@ -1,21 +1,53 @@
-use blockchain_file::blocks::Block;
 use blockchain_hooks::ApplicationState;
 use blockchain_protocol::BlockchainProtocol;
 use blockchain_protocol::payload::FoundBlockPayload;
 
 use hooks::State;
 
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
 pub fn on_found_block(state: ApplicationState<State>) {
     let message = BlockchainProtocol::<FoundBlockPayload>::from_bytes(&state.payload_buffer).expect("Parsing the protocol should be successful");
     event!("FOUND_BLOCK {:?}", message.payload);
 
-    Block::init();
-    let mut block = Block::new();
-    block.index = message.payload.index;
-    block.content = message.payload.content;
-    block.timestamp = message.payload.timestamp;
-    block.nonce = message.payload.nonce;
-    block.prev = message.payload.prev;
-    block.hash = message.payload.hash;
-    block.save();
+    create_folder();
+    save_file(
+        message.payload.index,
+        message.payload.content, 
+        message.payload.timestamp,
+        message.payload.nonce,
+        message.payload.prev,
+        message.payload.hash);
+}
+
+fn create_folder() {
+    if !Path::new("./block_data").exists() {
+        fs::create_dir("./block_data").unwrap();
+    }
+}
+
+fn save_file(index: u64, content: String, timestamp: i64, nonce: u64, prev: String, hash: String) {
+    let mut filename = String::from("");
+
+    for i in 0..16 {
+        filename = filename + &hash.chars().nth(48 + i).unwrap().to_string();
+    }
+
+    let mut file = File::create(format!("{}/{}", "./block_data", filename))
+        .expect("Could not write block file.");
+    file.write_all(
+        format!(
+            "{}\n{}\n{}\n{}\n{}\n{}", 
+            index, 
+            content, 
+            timestamp, 
+            nonce, 
+            prev, 
+            hash
+        )
+        .as_bytes())
+    .expect("Error writing block information");
 }
