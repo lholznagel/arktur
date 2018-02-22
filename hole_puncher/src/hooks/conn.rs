aa72887daa9b8aac1636e5a2d4dd6daefd500db8
lholznagel <github@lholznagel.info>
2018-02-22 18:59:58 +0100

    hole_puncher - Hole puncher cleanup

--- hole_puncher/src/hooks/conn.rs
+++ hole_puncher/src/hooks/conn.rs
@@ -1,20 +1,22 @@
 use blockchain_hooks::{ApplicationState, EventCodes};
 use blockchain_protocol::BlockchainProtocol;
-use blockchain_protocol::enums::status::StatusCodes;
-use blockchain_protocol::payload::{HolePuncherConn, RegisterAckPayload, PingPayload};
+use blockchain_protocol::payload::HolePuncherConn;
 
 use hooks::State;
 
-use std::{thread, time}
-
-pub fn on_holepuncher_conn(state: ApplicationState<State>) {
-    event!("HOLE_PUNCHER_CONN");
-    let message = BlockchainProtocol::<HolePuncherConn>::from_bytes(&state.payload_buffer).unwrap();
+pub fn on_hole_puncher_conn(state: ApplicationState<State>) {
+    let message = BlockchainProtocol::<HolePuncherConn>::from_bytes(&state.payload_buffer)
+        .expect("Parsing the protocol should be successful.");
 
     let payload = HolePuncherConn {
         address: state.source
     };
 
-    let result = BlockchainProtocol::<HolePuncherConn>::new().set_payload(payload).set_event_code(EventCodes::HolePuncherConn).build();
-    state.udp.send_to(&result, message.payload.address).expect("Sending a message should be successful");
+    let result = BlockchainProtocol::<HolePuncherConn>::new()
+        .set_payload(payload)
+        .set_event_code(EventCodes::HolePuncherConn)
+        .build();
+
+    state.udp.send_to(&result, message.payload.address)
+        .expect("Sending using UDP should be successful.");
 }
--- hole_puncher/src/hooks/explore_network.rs
+++ hole_puncher/src/hooks/explore_network.rs
@@ -6,13 +6,16 @@ use blockchain_protocol::enums::status::StatusCodes;
 use hooks::State;
 
 pub fn on_explore_network(state: ApplicationState<State>) {
-    debug!("Sending peers to debugger");
-    let state_lock = state.state.lock().expect("Locking the mutex should be successful.");
+    let state_lock = state.state.lock()
+        .expect("Locking the mutex should be successful.");
 
     let answer = BlockchainProtocol::new()
         .set_event_code(EventCodes::ExploreNetwork)
         .set_status_code(StatusCodes::Ok)
         .set_payload(ExploreNetworkPayload::new().set_peers(state_lock.peers.clone()))
         .build();
-    state.udp.send_to(&answer, state.source).expect("Sending a response should be successful");
+
+    info!("Sending Debugger all peers.");
+    state.udp.send_to(&answer, state.source)
+        .expect("Sending using UDP should be successful.");
 }
